c917b3b739cf716ab071d22cfc47bd371d031cef
joday <iddqfd5@naver.com>
2022-06-30 21:57:17 +0900

    POST 값전달후 db에 저장

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
+from accountapp.models import hi
+
 # Create your views here.
 
 def index(request):
     
     if request.method == "POST":
-        return render(request, 'accountapp/hi.html',context={'text':'POST요청완료'})
+
+        temp = request.POST.get('hi_input')
+        new_hi = hi()
+        new_hi.text = temp
+        new_hi.save()
+
+
+
+        return render(request, 'accountapp/hi.html',context={'new_hi':new_hi})
 
 
 
