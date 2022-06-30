5f2ea51d8eb358a3f038ebf4c98217498bb7e34e
joday <iddqfd5@naver.com>
2022-06-30 21:50:03 +0900

    get,post 요청 받고 값전달

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'accountapp/hi.html')
+    
+    if request.method == "POST":
+        return render(request, 'accountapp/hi.html',context={'text':'POST요청완료'})
+
+
+
+
+    return render(request, 'accountapp/hi.html',context={'text':'GET요청완료'})
