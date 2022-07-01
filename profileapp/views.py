8cc33dce6d77b24e4d60e22ee8dc43e33cfc68e0
joday <iddqfd5@naver.com>
2022-07-01 18:31:29 +0900

    업데이트 뷰의 본인만 참조하도록 데코레이터 작성

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
+from profileapp.decorators import profile_ownership_required
 
 # Create your views here.
 from profileapp.forms import ProfileCreateForm
@@ -21,3 +22,13 @@ class ProfileCreateView(CreateView):
         temp.user = self.request.user
         temp.save()
         return super().form_valid(form)
+
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileCreateForm
+    context_object_name = 'target_profile'
+    success_url = reverse_lazy('accountapp:hi')
+    template_name = 'profileapp/update.html'
