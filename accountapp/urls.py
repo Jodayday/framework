b0ee3077a6d222ac55a270f059c18efef0d8c632
joday <iddqfd5@naver.com>
2022-07-01 16:00:38 +0900

    detailview를 사용

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
 
-from accountapp.views import index
+from accountapp.views import AccountDetailView, index
 from accountapp.views import AccountCreateView
 from django.contrib.auth.views import LoginView, LogoutView
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hi/',index, name='hi'),
-    path('login/',LoginView.as_view(template_name='accountapp/login.html'), name='login'),
-    path('logout/',LogoutView.as_view(), name='logout'),
-    path('create/',AccountCreateView.as_view(), name='create'),
-
+    path('hi/', index, name='hi'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('create/', AccountCreateView.as_view(), name='create'),
+    path('detail/<int:pk>/', AccountDetailView.as_view(), name='detail'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -9,8 +9,9 @@ from accountapp.models import hi
 
 # Create your views here.
 
+
 def index(request):
-    hi_list=hi.objects.all().order_by('-pk')
+    hi_list = hi.objects.all().order_by('-pk')
     if request.method == "POST":
 
         temp = request.POST.get('hi_input')
@@ -18,10 +19,17 @@ def index(request):
         new_hi.text = temp
         new_hi.save()
         return redirect(reverse('accountapp:hi'))
-    return render(request, 'accountapp/hi.html',context={'hi_list':hi_list})
+    return render(request, 'accountapp/hi.html', context={'hi_list': hi_list})
+
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hi')
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
