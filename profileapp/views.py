04c54bfd26654da9ac78cc0b6dd149235545dfdc
joday <iddqfd5@naver.com>
2022-07-01 18:12:46 +0900

    생성한 form를 이용하여 view 사용, 뷰에서 form에서 받은 값에 추가하여 전달, form에 이미지 전달시 enctype 설정 필요

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from django.views.generic import CreateView, UpdateView
 
 # Create your views here.
+from profileapp.forms import ProfileCreateForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreateForm
+    context_object_name = 'target_profile'
+    success_url = reverse_lazy('accountapp:hi')
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        """form에서 제출한 값이 여기로 온다. """
+        temp = form.save(commit=False)
+        temp.user = self.request.user
+        temp.save()
+        return super().form_valid(form)
